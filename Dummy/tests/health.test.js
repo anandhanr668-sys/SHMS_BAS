6f3bb0648cf75664ad31359b3bb93e180a5fdd4d
Anandhan_R <anandhanr668@gmail.com>
2026-02-01 11:29:56 +0530

    removed old

--- Dummy/tests/health.test.js
+++
@@ -1,9 +0,0 @@
-import request from "supertest";
-import app from "../src/app.js";
-
-describe("Health Check", () => {
-  it("should return OK", async () => {
-    const res = await request(app).get("/health");
-    expect(res.statusCode).toBe(200);
-  });
-});
